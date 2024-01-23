589358866a428c4731db8a6468c5fe596a7e148f
shaxboz-1805 <islomovshaxboz740@gmail.com>
2024-01-23 20:59:12 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k*j9&+0b*$ean625v9e5%ih(4i-2&+qm@+z_&dzki7$ko$2@_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
